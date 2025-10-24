df093e289c7ee569209d5e0a4754a8aab4d6c551
dimszky <schooldims@gmail.com>
2025-10-24 15:46:07 +0700

    first commit

---
+++ backend/src/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+const path = require('path');
+
+const app = express();
+
+// === CORS ===
+
+// === Static File Serving ===
+
+// === IMPORT ROUTES ===
+
+// === Register semua modular route ===
+
+// === Error Handling ===
